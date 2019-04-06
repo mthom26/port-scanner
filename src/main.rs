539b8572036c381da0ef3e0637a426a7a2601238
mthom26 <mthom2626@gmail.com>
2019-04-06 14:38:09 +0100

    Add  crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate clap;
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Port Scanner")
+        .arg(Arg::with_name("url")
+                .help("The URL to scan")
+                .required(true))
+        .get_matches();
+
+    println!("{:?}", matches);
 }
