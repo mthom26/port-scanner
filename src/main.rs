d8686e6bfed6ab2c93b0c7fbfaa63958a721bb16
mthom26 <mthom2626@gmail.com>
2019-04-06 15:47:21 +0100

    Convert url into socket address

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 extern crate clap;
+
+use std::net::{ToSocketAddrs, IpAddr};
+use std::io::{Error};
+use std::process;
 use clap::{App, Arg};
 
 fn main() {
@@ -9,4 +13,20 @@ fn main() {
         .get_matches();
 
     println!("{:?}", matches);
+
+    let url = matches.value_of("url").unwrap();
+    println!("Url: {}", url);
+    
+    // Convert url to socket address
+    let address: Result<Vec<IpAddr>, Error> = (url, 0).to_socket_addrs().map(|iter| {
+        iter.map(|addr| addr.ip()).collect()
+    });
+    
+    // Exit program if address is an error
+    if address.is_err() {
+        println!("{:?}", address);
+        process::exit(1);
+    }
+
+    println!("{:?}", address);
 }
