722f537f799cfdc8186feb7b6b67031150ade8d1
mthom26 <mthom2626@gmail.com>
2019-04-06 18:58:40 +0100

    Add `scan()`
    Add `scan()` function and print vector of open ports.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 extern crate clap;
 
-use std::net::{ToSocketAddrs, IpAddr};
+use std::net::{ToSocketAddrs, IpAddr, TcpStream};
 use std::io::{Error};
 use std::process;
+use std::sync::mpsc::{Sender, channel};
+use std::thread;
 use clap::{App, Arg};
+
+const MAX_PORT: u16 = 65535;
 
 fn main() {
     let matches = App::new("Port Scanner")
@@ -28,5 +32,46 @@ fn main() {
         process::exit(1);
     }
 
+    let address = address.unwrap();
     println!("{:?}", address);
+
+    let num_threads = 100;
+    let (tx, rx) = channel();
+    for index in 0..num_threads {
+        let tx = Sender::clone(&tx);
+        // TODO - maybe send reference of address to 'scan()' instead? 
+        let addr = address.clone()[0];
+        thread::spawn(move || {
+            scan(tx, index, addr, num_threads);
+        });
+    }
+
+    let mut output = Vec::new();
+    drop(tx);
+
+    for port in rx {
+        output.push(port);
+    }
+
+    println!("Output: {:?}", output);
+}
+
+fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+    let mut current_port = start_port + 1;
+    loop {
+        match TcpStream::connect((addr, current_port)) {
+            Ok(_) => {
+                // Connection succeeded, send port number
+                tx.send(current_port).unwrap();
+            },
+            Err(_) => {
+                // connection failed, nothing to do
+            }
+        }
+
+        if (MAX_PORT - current_port) <= num_threads {
+            break;
+        }
+        current_port += num_threads;
+    }
 }
